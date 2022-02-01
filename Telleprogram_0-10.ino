40064f6af7bba74fc790089a1091bbf348d40aa7
Lutfisk1 <lukruskatrud@gmail.com>
2022-02-01 08:39:20 +0100

    Veldig kult telleprogram
    Telleprogram

---
+++ Telleprogram_0-10.ino
@@ -0,0 +1,16 @@
+
+void setup (){
+  Serial.begin(9600);
+  
+void loop
+  int potVal = analogRead(3);
+  potVal = map(potval, 0, 1023, 0 255);
+  analogWrite(6, potVal);
+
+  // 0 --> 1023
+  // 0 --> 255
+
+  voltage = map(potValue, 0, 255, 0, 5);
+  Serial.println (potVal);
+  Serial.println ("  ");
+  Serial.println(voltage);
